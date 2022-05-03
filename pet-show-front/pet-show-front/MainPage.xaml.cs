9a90cbfd88b1bbb37c476815b09d5d17a949dbf0
bruno <bruno@NITROBRUNO>
2022-05-03 19:13:38 -0300

    Add project files.

---
+++ pet-show-front/pet-show-front/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace pet_show_front
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
